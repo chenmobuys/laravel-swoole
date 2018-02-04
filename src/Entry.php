9842a861845fea4e6b3ac9f4e2bebc049cae570a
chenmobuys <33781280+chenmobuys@users.noreply.github.com>
2018-02-04 15:35:17 +0800

    Update Entry.php

--- src/Entry.php
+++ src/Entry.php
@@ -2,15 +2,23 @@
 
 if(file_exists(__DIR__ . '/../../../vendor/autoload.php')){
   require __DIR__ . '/../../../vendor/autoload.php';
+}else{
+  echo 1;
 }
 
 if(file_exists(__DIR__ . '/../../vendor/autoload.php')){
   require __DIR__ . '/../../vendor/autoload.php';
+}else{
+  echo 2;
 }
 
 if(file_exists(__DIR__ . '/../vendor/autoload.php')){
   require __DIR__ . '/../vendor/autoload.php';
+}else{
+  echo 3;
 }
+
+die;
 
 $input = file_get_contents('php://stdin');
 
