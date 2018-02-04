d84d68a51b3c91d03f018fa654fe46a5286c5018
chenmobuys <33781280+chenmobuys@users.noreply.github.com>
2018-02-04 15:37:40 +0800

    Update Entry.php

--- src/Entry.php
+++ src/Entry.php
@@ -1,26 +1,8 @@
 <?php
 
-echo __DIR__;
-
-if(file_exists(__DIR__ . '/../../../vendor/autoload.php')){
-  require __DIR__ . '/../../../vendor/autoload.php';
-}else{
-  echo 1;
+if(file_exists(__DIR__ . '/../../../autoload.php')){
+  require __DIR__ . '/../../../autoload.php';
 }
-
-if(file_exists(__DIR__ . '/../../vendor/autoload.php')){
-  require __DIR__ . '/../../vendor/autoload.php';
-}else{
-  echo 2;
-}
-
-if(file_exists(__DIR__ . '/../vendor/autoload.php')){
-  require __DIR__ . '/../vendor/autoload.php';
-}else{
-  echo 3;
-}
-
-die;
 
 $input = file_get_contents('php://stdin');
 
