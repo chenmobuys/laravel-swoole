909e6c60e0d0a5215a1fa58dd3fb907f424d9bfb
chenmobuys <33781280+chenmobuys@users.noreply.github.com>
2018-02-04 15:33:15 +0800

    Update Entry.php

--- src/Entry.php
+++ src/Entry.php
@@ -8,6 +8,10 @@ if(file_exists(__DIR__ . '/../../vendor/autoload.php')){
   require __DIR__ . '/../../vendor/autoload.php';
 }
 
+if(file_exists(__DIR__ . '/../vendor/autoload.php')){
+  require __DIR__ . '/../vendor/autoload.php';
+}
+
 $input = file_get_contents('php://stdin');
 
 $configs = unserialize($input);
